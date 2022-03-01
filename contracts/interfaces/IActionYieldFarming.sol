a5e39724c4d5ec142bcffb2074d144df842523da
zgljl2012 <2693491512@qq.com>
2022-03-01 20:46:21 +0800

    action yield farming interface and token interface

---
+++ contracts/interfaces/IActionYieldFarming.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+
+interface IActionYieldFarming {
+    /**
+     * The event emitted after trigger an action
+     * @param address the address should be rewarded
+     * @param uint the action ID
+     * @param uint256 reward amount
+     */
+    event ActionYieldEvent(address, uint, uint256);
+
+    /**
+     * Yield to an action
+     * @param address The address should be rewarded
+     * @param uint The action of the action, definit in ../lib/Actions.sol Action
+     * @return the amount of rewards
+     * Emits {ActionYieldEvent}
+     */
+    function actionYield(address, uint) external returns (uint256);
+}
---
+++ contracts/interfaces/IActionYieldFarmingAdmin.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.24;
+
+interface IActionYieldFarmingAdmin {
+    /**
+     * setActionRewardEvent
+     * @param owner address
+     * @param uint action ID
+     * @param uint256 reward per actions
+     */
+    event setActionRewardEvent(address, uint, uint256);
+
+    /**
+     * setActionMaxYieldTimesEvent
+     * @param owner address
+     * @param uint action ID
+     * @param uint max yield times
+     */
+    event setActionMaxYieldTimesEvent(address, uint, uint);
+
+    /**
+     * Set the reward per actions
+     * 设置行为的单次激励数量
+     * @param uint action ID
+     * @param uint256 rewards
+     * Emits {setActionRewardEvent}
+     */
+    function setActionReward(uint, uint256) external;
+
+    /**
+     * Set the max reward times per actions
+     * 设置行为的总激励次数
+     * @param uint action ID
+     * @param uint max reward times
+     * Emits {setActionMaxYieldTimesEvent}
+     */
+    function setActionMaxYieldTimes(uint, uint) external;
+
+    /**
+     * Get current reward times of an action
+     * 获取某行为当前已激励次数
+     * @param uint action ID
+     * @return times
+     */
+    function getCurrentYieldTimesOfAction(uint) external view returns (uint);
+
+    /**
+     * Get reward of an action
+     * 获取某行为奖励一次的金额
+     * @param uint action ID
+     * @return reward
+     */
+    function getActionReward(uint) external view returns (uint256);
+
+    /**
+     * Get the max reward times of an action
+     * 获取某行为最大激励次数
+     * @param uint action ID
+     * @return times
+     */
+    function getMaxYieldTimesOfAction(uint) external view returns (uint);
+
+    /**
+     * Get all yield rewards of an action now
+     * 获取某行为当前发出的总激励
+     * @param uint action ID
+     */
+    function getCurrentYieldRewardsOfAction(uint) external view returns (uint256);
+
+    /**
+     * Get all rewards of all action now
+     * 获取所有行为的已激励总额
+     * @return all rewards now
+     */
+    function getCurrentRewardsOfAllAction() external view returns (uint256);
+
+    /**
+     * Get max yield rewards of an action
+     * 获取某行为可发出的总激励
+     * @param uint action ID
+     * @return rewards
+     */
+    function getMaxYieldRewardsOfAction(uint) external view returns (uint256);
+
+    /**
+     * Get max rewards of all action
+     * 获取所有行为可发出的总激励
+     * @return max rewards
+     */
+    function getMaxRewardsOfAllAction() external view returns (uint256);
+}
